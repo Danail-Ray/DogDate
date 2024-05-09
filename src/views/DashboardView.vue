d8618b8363278334d470a081c76bb13af4e620e5
SebastianM1999 <sebastianmissler@web.de>
2024-05-09 22:44:53 +0200

    profile changes

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,118 +4,122 @@
 
     <!-- asdasdasd -->
 
-    <div class="main main-raised">
-      <div class="profile-content">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 mx-auto">
-              <div class="profile text-center">
-                <div class="avatar">
-                  <img
-                    src="https://preview.redd.it/transform-your-selfie-into-a-stunning-ai-avatar-with-stable-v0-t1mdnkadob6a1.png?width=1024&format=png&auto=webp&s=07b4edf62cd53cbd30886482090cb7e7c9c16372"
-                    alt="Circle Image"
-                    class="img-raised rounded-circle img-fluid w-25"
-                  />
-                </div>
-                <div class="name">
-                  <h3 class="title">{{username}}</h3>
-                  <h6>Senior Fullstack Developer</h6>
-                  <a href="#pablo" class="btn btn-just-icon btn-link btn-dribbble"
-                    ><i class="fa fa-dribbble"></i
-                  ></a>
-                  <a href="#pablo" class="btn btn-just-icon btn-link btn-twitter"
-                    ><i class="fa fa-twitter"></i
-                  ></a>
-                  <a href="#pablo" class="btn btn-just-icon btn-link btn-pinterest"
-                    ><i class="fa fa-pinterest"></i
-                  ></a>
-                </div>
-              </div>
+    <div class="profile-card-bg">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <div class="profile text-center">
+            <div class="avatar">
+              <img
+                src="https://preview.redd.it/transform-your-selfie-into-a-stunning-ai-avatar-with-stable-v0-t1mdnkadob6a1.png?width=1024&format=png&auto=webp&s=07b4edf62cd53cbd30886482090cb7e7c9c16372"
+                alt="Circle Image"
+                class="img-raised rounded-circle"
+                style="width: 150px; height: auto"
+              />
             </div>
-          </div>
-          <div class="description text-center">
-            <p>
-              An artist of considerable range, Chet Faker — the name taken by Melbourne-raised,
-              Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving
-              it a warm, intimate feel with a solid groove structure.
-            </p>
-          </div>
-          <div class="row">
-            <div class="col-md-6 ml-auto mr-auto">
-              <div class="profile-tabs">
-                <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#studio" role="tab" data-toggle="tab">
-                      <i class="material-icons">camera</i>
-                      Studio
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#works" role="tab" data-toggle="tab">
-                      <i class="material-icons">palette</i>
-                      Work
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#favorite" role="tab" data-toggle="tab">
-                      <i class="material-icons">favorite</i>
-                      Favorite
-                    </a>
-                  </li>
-                </ul>
-              </div>
+            <div class="name">
+              <h3 class="title">{{ username }}</h3>
+              <h6>Senior Fullstack Developer</h6>
+              <a href="#pablo" class="btn btn-just-icon btn-link btn-dribbble"
+                ><i class="fa fa-dribbble"></i
+              ></a>
+              <a href="#pablo" class="btn btn-just-icon btn-link btn-twitter"
+                ><i class="fa fa-twitter"></i
+              ></a>
+              <a href="#pablo" class="btn btn-just-icon btn-link btn-pinterest"
+                ><i class="fa fa-pinterest"></i
+              ></a>
             </div>
-          </div>
-          <a class="btn btn-primary" href="#" role="button">Link</a>
-
-          <div class="tab-content tab-space">
-            <div class="tab-pane active text-center gallery" id="studio">
-              <div class="row">
-                <div class="col-md-3">
-                  <img
-                    src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80"
-                    class="rounded img-fluid mb-3"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1528249227670-9ba48616014f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b8e7db17b83084f16fdeadfc93b95b&auto=format&fit=crop&w=357&q=80"
-                    class="rounded img-fluid"
-                  />
-                </div>
-                <div class="col-md-3">
-                  <img
-                    src="https://images.unsplash.com/photo-1521341057461-6eb5f40b07ab?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=72da2f550f8cbd0ec252ad6fb89c96b2&auto=format&fit=crop&w=334&q=80"
-                    class="rounded img-fluid mb-3"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80"
-                    class="rounded img-fluid"
-                  />
-                </div>
-                <div class="col-md-3">
-                  <img
-                    src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80"
-                    class="rounded img-fluid mb-3"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80"
-                    class="rounded img-fluid"
-                  />
-                </div>
-                <div class="col-md-3">
-                  <img
-                    src="https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec2bdc92a9687b6af5089b335691830e&auto=format&fit=crop&w=750&q=80"
-                    class="rounded img-fluid mb-3"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1504346466600-714572c4b726?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6754ded479383b7e3144de310fa88277&auto=format&fit=crop&w=750&q=80"
-                    class="rounded img-fluid"
-                  />
-                </div>
-              </div>
-            </div>
-            <!-- Add other tab-panes as needed -->
           </div>
         </div>
+      </div>
+      <div class="description text-center">
+        <p>
+          An artist of considerable range, Chet Faker — the name taken by Melbourne-raised,
+          Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it
+          a warm, intimate feel with a solid groove structure.
+        </p>
+      </div>
+      <div class="row">
+        <div class="col-md-6 ml-auto mr-auto">
+          <div class="profile-tabs">
+            <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link" href="#favorite" role="tab" data-toggle="tab">
+                  <i class="material-icons">favorite</i>
+                  Favorite
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#works" role="tab" data-toggle="tab">
+                  <i class="material-icons">forum</i>
+                  Chat
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="tab-content tab-space">
+        <div class="tab-pane active text-center gallery" id="studio">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80"
+                  class="rounded img-fluid mb-3"
+                />
+              </div>
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1528249227670-9ba48616014f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=66b8e7db17b83084f16fdeadfc93b95b&auto=format&fit=crop&w=357&q=80"
+                  class="rounded img-fluid"
+                />
+              </div>
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1521341057461-6eb5f40b07ab?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=72da2f550f8cbd0ec252ad6fb89c96b2&auto=format&fit=crop&w=334&q=80"
+                  class="rounded img-fluid mb-3"
+                />
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80"
+                  class="rounded img-fluid"
+                />
+              </div>
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ec2bdc92a9687b6af5089b335691830e&auto=format&fit=crop&w=750&q=80"
+                  class="rounded img-fluid mb-3"
+                />
+              </div>
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1504346466600-714572c4b726?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6754ded479383b7e3144de310fa88277&auto=format&fit=crop&w=750&q=80"
+                  class="rounded img-fluid"
+                />
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1524498250077-390f9e378fc0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=83079913579babb9d2c94a5941b2e69d&auto=format&fit=crop&w=751&q=80"
+                  class="rounded img-fluid mb-3"
+                />
+              </div>
+              <div class="p-3">
+                <img
+                  src="https://images.unsplash.com/photo-1506667527953-22eca67dd919?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6326214b7ce18d74dde5e88db4a12dd5&auto=format&fit=crop&w=750&q=80"
+                  class="rounded img-fluid"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Add other tab-panes as needed -->
       </div>
     </div>
   </div>
@@ -180,9 +184,9 @@ body {
   font-family: 'Poppins', sans-serif;
 }
 
-Header {
+/* Header {
   padding: 200px;
-}
+} */
 .background {
   min-height: 100vh;
   background: url(../assets/home-bg.jpg) no-repeat;
@@ -194,8 +198,13 @@ html * {
   -webkit-font-smoothing: antialiased;
 }
 
-.container {
-  margin-top: 10vh;
+.profile-card-bg {
+  margin: 5vh 10vw 0 10vw;
+  padding-top: 50px;
+  padding-left: 50px;
+  padding-right: 50px;
+  border-radius: 25px;
+  background-color: #63636337;
 }
 
 .h6,
@@ -254,21 +263,6 @@ a .material-icons {
   background: rgba(0, 0, 0, 0.5);
 }
 
-.main-raised {
-  margin: -60px 30px 0;
-  border-radius: 6px;
-  box-shadow:
-    0 16px 24px 2px rgba(0, 0, 0, 0.14),
-    0 6px 30px 5px rgba(0, 0, 0, 0.12),
-    0 8px 10px -5px rgba(0, 0, 0, 0.2);
-}
-
-.main {
-  background: #4b4b4bba;
-  position: relative;
-  z-index: 3;
-}
-
 .profile-page .profile {
   text-align: center;
 }
@@ -285,9 +279,7 @@ a .material-icons {
 }
 
 .img-raised {
-  box-shadow:
-    0 5px 15px -8px rgba(0, 0, 0, 0.24),
-    0 8px 10px -5px rgba(0, 0, 0, 0.2);
+  box-shadow: 10px 10px 10px rgba(33, 33, 33, 0.7);
 }
 
 .rounded-circle {
@@ -304,7 +296,7 @@ a .material-icons {
   margin-top: 30px;
   margin-bottom: 25px;
   min-height: 32px;
-  color: #fbd784;
+  color: #ffd061;
   font-weight: 700;
   font-family: 'Roboto Slab', 'Times New Roman', serif;
 }
@@ -317,8 +309,8 @@ a .material-icons {
 }
 
 p {
-  font-size: 14px;
-  margin: 0 0 10px;
+  font-size: 16px;
+  margin: 10px 0 10px 0;
 }
 
 .profile-page .profile-tabs {
@@ -328,17 +320,18 @@ p {
 .nav-pills,
 .nav-tabs {
   border: 0;
-  border-radius: 3px;
-  padding: 0 15px;
+  border-radius: 4px;
+  padding: 15px 15px;
 }
 
 .nav .nav-item {
   position: relative;
-  margin: 0 2px;
+  margin: 0 10px;
 }
 
 .nav-pills.nav-pills-icons .nav-item .nav-link {
   border-radius: 4px;
+  color: #fff;
 }
 
 .nav-pills .nav-item .nav-link.active {
